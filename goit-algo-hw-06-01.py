d018baa9774920209acc4e2838a72e28e5c21397
dmytrolazar <lazar.dmytro@gmail.com>
2024-11-03 20:49:03 +0200

    Added DFS, BFS and dijkstra algorithms.

--- goit-algo-hw-06-01.py
+++ goit-algo-hw-06-01.py
@@ -1,7 +1,56 @@
+import math
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
+
+def dfs_traverse(graph: nx.Graph, start_vertex, depth_limit=math.inf):
+    visited = set()
+    order = []
+    stack = [(start_vertex, 0)]
+
+    while stack:
+        vertex, depth = stack.pop()
+        if vertex not in visited and depth <= depth_limit:
+            order.append(vertex)
+            visited.add(vertex)
+            stack.extend([(n, depth + 1) for n in set(graph.neighbors(vertex)) - visited])
+            
+    return order
+
+def bfs_traverse(graph: nx.Graph, start_vertex, depth_limit=math.inf):
+    visited = set()
+    order = []
+    queue = deque()
+    queue.append((start_vertex, 0))
+
+    while queue:
+        vertex, depth = queue.popleft()
+        if vertex not in visited and depth <= depth_limit:
+            order.append(vertex)
+            visited.add(vertex)
+            queue.extend([(n, depth + 1) for n in set(graph.neighbors(vertex)) - visited])
+
+    return order
+
+def dijkstra(graph: nx.Graph, start_vertex):
+    distances = {vertex: float('infinity') for vertex in graph.nodes()}
+    distances[start_vertex] = 0
+    unvisited = list(graph.nodes())
+
+    while unvisited:
+        current_vertex = min(unvisited, key=lambda vertex: distances[vertex])
+        if distances[current_vertex] == float('infinity'):
+            break
+        for neighbor, weight in [(n, graph.get_edge_data(n, current_vertex)['weight']) for n in graph.neighbors(current_vertex)]:
+            distance = distances[current_vertex] + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+        unvisited.remove(current_vertex)
+
+    return distances
 
 G = nx.Graph()
+
 with open('data.txt', 'r', encoding='utf8') as file:
     previous_line = ""
     for line in file:
@@ -20,22 +69,13 @@ print(f"Кількість ребер у графі:  {G.number_of_edges()}.")
 print(f"Близькість вузлів у графі в порядку зростання:\n\
 {'\n'.join("    {}: {}".format(k, v) for k, v in dict(sorted(nx.closeness_centrality(G).items(), key=lambda item: item[1])).items())}")
 
-# DFS
-dfs_tree = nx.dfs_tree(G, source='Пасічна', depth_limit=5)
-print(f"Шлях від зупинка Пасічна з максимальною глибиною 5 алгоритму DFS: {', '.join(list(dfs_tree.nodes()))}")
-nx.draw(dfs_tree, with_labels=True)
-plt.show()
-
-# BFS
-bfs_tree = nx.bfs_tree(G, source='Пасічна', depth_limit=5)
-print(f"Шлях від зупинка Пасічна з максимальною глибиною 5 алгоритму BFS: {', '.join(list(bfs_tree.nodes()))}")
-nx.draw(bfs_tree, with_labels=True)
-plt.show()
-
-nx.draw(G, with_labels=True)
-plt.show()
-
-# алгоритм Дейкстри для знаходження найкоротшого шляху в розробленому графі між всіма ребрами
+print(f"Шлях від зупинка Пасічна з максимальною глибиною 5 алгоритму DFS: {dfs_traverse(G, 'Пасічна', 5)}")
+print(f"Шлях від зупинка Пасічна з максимальною глибиною 5 алгоритму BFS: {bfs_traverse(G, 'Пасічна', 5)}")
 nx.set_edge_attributes(G, values = 1, name = 'weight')
-length = dict(nx.all_pairs_dijkstra_path_length(G))
+distances = dijkstra(G, 'Пасічна')
+print(f"Результат роботи алгоритму Дейкстри для знаходження найкоротших шляхів між всіма вершинами графа: {distances}")
+
+# nx.draw(G, with_labels=True)
+# plt.show()
+
 
